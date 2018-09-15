cf002fda589c7d89f178412ef220c345fd144452
HI123123 <justinchen2012@gmail.com>
2018-09-15 14:34:28 -0400

    WebScraper changes

--- app/src/main/java/backend/WebScraper.java
+++ app/src/main/java/backend/WebScraper.java
@@ -1,4 +1,31 @@
 package backend;
+import org.jsoup.Jsoup;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 
 public class WebScraper {
+    ArrayList<String> categories;
+    ArrayList<String> websites = new ArrayList<String>();
+
+    public WebScraper(ArrayList<String> l){
+        categories = l;
+
+    }
+    public void fillWebsites(){
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("Websites"));
+            while(reader.readLine()!=null&&reader.readLine().charAt(0)){
+
+            }
+
+
+        }catch (IOException e) {
+
+        }
+
+    }
 }
