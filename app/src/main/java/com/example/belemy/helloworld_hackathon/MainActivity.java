205770763b68bf3fc5da22a38d994ef5a182137f
chirayu <garg104@purdue.edu>
2018-09-16 10:42:37 -0400

    addedmusic

--- app/src/main/java/com/example/belemy/helloworld_hackathon/MainActivity.java
+++ app/src/main/java/com/example/belemy/helloworld_hackathon/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.belemy.helloworld_hackathon;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent music = new Intent();
+        music.setClass(this, MusicService.class);
+        startService(music);
         setContentView(R.layout.activity_main);
 
 
@@ -40,4 +47,31 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         //}
     }//nextactivity
+
+    private boolean mIsBound = false;
+    private MusicService mServ;
+    private ServiceConnection Scon = new ServiceConnection() {
+
+        public void onServiceConnected(ComponentName name, IBinder
+                binder) {
+            mServ = ((MusicService.ServiceBinder) binder).getService();
+        }
+
+        public void onServiceDisconnected(ComponentName name) {
+            mServ = null;
+        }
+    };
+
+    void doBindService() {
+        bindService(new Intent(this, MusicService.class),
+                Scon, Context.BIND_AUTO_CREATE);
+        mIsBound = true;
+    }
+
+    void doUnbindService() {
+        if (mIsBound) {
+            unbindService(Scon);
+            mIsBound = false;
+        }
+    }
 }//class
